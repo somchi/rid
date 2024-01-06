afeba199bce5a81c33304378893288e03e9ba836
somchi <chisomike11@gmail.com>
2024-01-06 02:40:32 +0100

    .

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,4 +1,4 @@
-import { useContext, useMemo } from 'react';
+import { useContext, useMemo, useState } from 'react';
 import { Event } from '../utils/types';
 import { EventCard } from './event-card';
 import { EventForm } from './eventForm';
@@ -8,23 +8,15 @@ import { EventView } from '../utils/helpers';
 import { Calendar, luxonLocalizer } from 'react-big-calendar';
 import { DateTime, Settings } from 'luxon';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { SET_EDIT, SET_OPEN_MODAL } from '../context/app.reducer';
 
 Settings.defaultZone = 'America/Los_Angeles';
 const localizer = luxonLocalizer(DateTime, { firstDayOfWeek: 7 });
 
 export const Content = () => {
-  const { state } = useContext(AppContext);
-
-  const formats = useMemo(
-    () => ({
-      dateFormat: 'dd',
-
-      dayFormat: (date: any, localizer: any) => {
-        return localizer.toJSDate(date);
-      },
-    }),
-    [],
-  );
+  const { state, dispatch } = useContext(AppContext);
+  const [warn, setWarn] = useState<boolean>(false);
+  const [data, setData] = useState<any>({} as any);
 
   const renderEventList = () => {
     const data = localStorage.getItem('events');
@@ -52,6 +44,36 @@ export const Content = () => {
     return data;
   }, []);
 
+  const handleDelete = () => {
+    setWarn(false);
+    const events = localStorage.getItem('events');
+    if (!events) return;
+
+    const parseData = JSON.parse(events);
+    const newEvents = parseData.filter(
+      (item: Event) => parseInt(item.id) !== parseInt(data.id),
+    );
+
+    localStorage.setItem('events', JSON.stringify(newEvents));
+    window.location.reload();
+  };
+
+  const handleWarn = () => {
+    setWarn(true);
+  };
+
+  const handleEdit = () => {
+    dispatch({
+      type: SET_EDIT,
+      payload: {
+        ...data,
+        startTime: DateTime.fromJSDate(data.startTime).toISO(),
+        endTime: DateTime.fromJSDate(data.endTime).toISO(),
+      },
+    });
+    dispatch({ type: SET_OPEN_MODAL, payload: true });
+  };
+
   return (
     <main>
       <Header />
@@ -59,14 +81,54 @@ export const Content = () => {
       {state.viewType === EventView.LIST ? (
         <div className="events-area">{renderEventList()}</div>
       ) : (
-        <div className="eventCalender">
-          <Calendar
-            localizer={localizer}
-            events={eventList}
-            startAccessor="startTime"
-            endAccessor="endTime"
-            formats={formats}
-          />
+        <div className="events-calender">
+          {Object.keys(data).length !== 0 ? (
+            <div className="event-card" style={{ top: 0 }}>
+              <div className="wrapper">
+                <span>{data.title}</span>
+                <span onClick={() => setData({})}>X</span>
+              </div>
+              <div className="time">
+                from:{' '}
+                <span>
+                  {DateTime.fromJSDate(data.startTime).toFormat('ff')}
+                </span>
+                To:{' '}
+                <span>{DateTime.fromJSDate(data.endTime).toFormat('ff')}</span>
+              </div>
+              <div className="action">
+                <button className="delete" onClick={handleWarn}>
+                  Delete
+                </button>
+                <button className="edit" onClick={handleEdit}>
+                  Edit
+                </button>
+              </div>
+            </div>
+          ) : null}
+          <div className="eventCalender">
+            <Calendar
+              localizer={localizer}
+              events={eventList}
+              startAccessor="startTime"
+              endAccessor="endTime"
+              onSelectEvent={(e) => setData(e)}
+            />
+          </div>
+
+          {warn ? (
+            <div className="delete-warn">
+              <span>Are you sure you what to delete {data.title}</span>
+              <div className="action">
+                <button className="delete" onClick={handleDelete}>
+                  Delete
+                </button>
+                <button className="edit" onClick={() => setWarn(false)}>
+                  cancel
+                </button>
+              </div>
+            </div>
+          ) : null}
         </div>
       )}
     </main>
